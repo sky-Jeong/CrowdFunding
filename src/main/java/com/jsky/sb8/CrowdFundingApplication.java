cc457c493d254196b13f94745298d8a68a31dea5
sky <skyjeong1962@gmail.com>
2020-12-14 15:06:17 +0900

    Initial commit

---
+++ src/main/java/com/jsky/sb8/CrowdFundingApplication.java
@@ -0,0 +1,13 @@
+package com.jsky.sb8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdFundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdFundingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsky/sb8/CrowdFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsky.sb8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdFundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
