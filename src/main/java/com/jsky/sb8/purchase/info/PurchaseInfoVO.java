ca27cbf74ab6bedfa258860f2bab06c678d2c155
sky <skyjeong1962@gmail.com>
2021-01-14 16:04:06 +0900

    Update step 2 page
    - Add postcode API

--- src/main/java/com/jsky/sb8/purchase/info/PurchaseInfoVO.java
+++ src/main/java/com/jsky/sb8/purchase/info/PurchaseInfoVO.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -57,6 +58,11 @@ public class PurchaseInfoVO {
 	private Timestamp ordered_at;
 	@Column
 	private String custom_data;
+	
+	@Transient
+	private String doro_addr;
+	@Transient
+	private String detail_addr;
 	
 	@OneToMany(mappedBy = "purchaseInfoVO", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<PurchaseVO> purchaseVOs;
---
+++ src/main/webapp/WEB-INF/views/common/postcode.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+
+<div id="layer" style="display:none;position:fixed;overflow:hidden;z-index:1;-webkit-overflow-scrolling:touch;">
+<img src="//t1.daumcdn.net/postcode/resource/images/close.png" id="btnCloseLayer"
+	style="cursor:pointer;position:absolute;right:-3px;top:-3px;z-index:1" onclick="closeDaumPostcode()" alt="닫기 버튼">
+</div>
